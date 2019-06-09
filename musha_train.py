a8be2273362557ddcb8fc8c670049b7e41aa2c8b
Xero-Hige <gaston.martinez.90@gmail.com>
2019-06-09 01:24:08 -0300

    Added musha trainer

---
+++ musha_train.py
@@ -0,0 +1,202 @@
+from __future__ import print_function
+
+from collections import namedtuple
+
+import retro
+import numpy as np
+import cv2
+import pickle
+import random
+
+from network_trainer import GameNetworkTrainer
+
+# random.seed(99)
+
+ENVIRONMENT = retro.make("MUSHA-Genesis", "Level1")
+
+State = namedtuple("State", "world lives score done reward")
+
+# ACTION: [0, 0, 0, 0, UP, DOWN, LEFT, RIGHT, JUMP, 0, 0, 0]
+
+ACTIONS = (
+    ("UP", [1, 0, 0, 0, 0]),
+    ("DOWN", [0, 1, 0, 0, 0]),
+    ("LEFT", [0, 0, 1, 0, 0]),
+    ("RIGHT", [0, 0, 0, 1, 0]),
+    ("SHOOT", [0, 0, 0, 0, 1]),
+    ("UP_SHOOT", [1, 0, 0, 0, 1]),
+    ("DOWN_SHOOT", [0, 1, 0, 0, 1]),
+    ("RIGHT_SHOOT", [0, 0, 1, 0, 1]),
+    ("LEFT_SHOOT", [0, 0, 0, 1, 1]),
+    ("NOTHING", [0] * 5)
+)
+
+
+def simulate(env, action, show=False):
+    if show:
+        env.render()
+
+    image, reward, done, _info = env.step([0] * 4 + action + [0] * 3)
+
+    score = _info["score"]
+    lives = _info["lives"]
+
+    return State(image, lives, score, done, reward)
+
+
+def calculate_reward(old_state, new_state):
+    if old_state.lives > new_state.lives:
+        return -1
+
+    return 1 if old_state.score < old_state.score else 0
+
+
+TRAIN_PLAYS = 100000
+TRAIN_CHECKPOINT = 5
+PLAY_DURATION = 1200
+TARGET_UPDATE = 10
+
+INITIAL_RANDOM_ACTION_RATE = 0.95
+DECAY_RATE = 0.001
+MIN_RANDOM_ACTION_RATE = 0.05
+
+SKIP_FRAMES = 3
+
+ACTIONS_SIZE = 5
+HISTORY_SIZE = 30
+
+# X_SPEED Y_SPEED XPOS YPOS RINGS SCORE END_X X_SCREEN Y_SCREEN
+OBSERVATION_SIZE = 9
+
+
+def train(store_path):
+    musha_trainer = GameNetworkTrainer(
+        possible_actions=len(ACTIONS),
+        observation_size=OBSERVATION_SIZE,
+        replays_memory=550
+    )
+
+    random_action_rate = INITIAL_RANDOM_ACTION_RATE + INITIAL_RANDOM_ACTION_RATE * DECAY_RATE
+
+    frames = 0
+
+    for episode in range(TRAIN_PLAYS):
+        musha_pilot = musha_trainer.get_policy_network()
+
+        ai_actions = 0
+
+        if random_action_rate > MIN_RANDOM_ACTION_RATE:
+            random_action_rate -= (random_action_rate * DECAY_RATE)
+        elif random_action_rate < MIN_RANDOM_ACTION_RATE:
+            random_action_rate = MIN_RANDOM_ACTION_RATE
+
+        if episode % TRAIN_CHECKPOINT == 0:
+            print("AI GAME ", end=" ")
+
+        env = ENVIRONMENT
+        env.reset()
+
+        old_state = simulate(env, ACTIONS[-1][1])
+
+        #
+        play_reward = 0
+        # ------------
+        nonaction = 0
+
+        while True:
+
+            frames = min(frames + 1, 550)
+
+            world_state = musha_pilot.process_state(old_state.world)
+
+            if episode % TRAIN_CHECKPOINT == 0 or random.random() > random_action_rate:
+                action = musha_pilot.get_best_action(world_state)
+                ai_actions += 1
+            else:
+                action = random.randint(0, len(ACTIONS) - 1)
+
+            action_desc, action_command = ACTIONS[action]
+
+            new_state = simulate(env, action_command, show=False)
+
+            for _ in range(SKIP_FRAMES):
+                new_state = simulate(env, action_command)
+
+            reward = calculate_reward(old_state, new_state)
+
+            if not reward:
+                nonaction += 1
+
+            play_reward += reward
+
+            new_world_state = musha_pilot.process_state(old_state.world)
+
+            musha_trainer.add_replay(world_state, action, reward, new_world_state)
+
+            old_state = new_state
+
+            if old_state.done or nonaction > 600:
+                break
+
+            if frames < 550:
+                continue
+
+            musha_trainer.train()
+
+        if episode % TARGET_UPDATE == 0:
+            musha_trainer.update_target()
+
+        print("Play", episode + 1, "reward", play_reward)
+        print("Action rate (", ai_actions / PLAY_DURATION, ")")
+
+        musha_trainer.store_policy(store_path + f"/musha_state_{episode // TRAIN_CHECKPOINT}")
+
+
+def play():
+    musha_trainer = GameNetworkTrainer(
+        possible_actions=len(ACTIONS),
+        observation_size=OBSERVATION_SIZE
+    )
+
+    musha_pilot = musha_trainer.get_policy_network()
+
+    env = ENVIRONMENT
+    env.reset()
+
+    old_state = simulate(env, ACTIONS[-1][1])
+
+    old_actions = [ACTIONS[-1][1] for _ in range(HISTORY_SIZE)]
+    print("playing")
+    #
+    play_reward = 0
+    # ------------
+    nonaction = 0
+
+    for frame in range(PLAY_DURATION):
+        world_state = musha_pilot.process_state(old_state.world)
+
+        action = musha_pilot.get_best_action(world_state)
+
+        action_desc, action_command = ACTIONS[action]
+
+        new_state = simulate(env, action_command, show=True)
+
+        for _ in range(SKIP_FRAMES):
+            new_state = simulate(env, action_command)
+
+        reward = calculate_reward(old_state, new_state)
+
+        if not reward:
+            nonaction += 1
+
+        play_reward += reward
+
+        old_actions.pop(0)
+        old_actions.append(action_command)
+
+        old_state = new_state
+
+        if old_state.done or nonaction > 600:
+            break
+
+    print("Play reward", play_reward)
--- network_trainer.py
+++ network_trainer.py
@@ -1,138 +1,121 @@
 import random
 from collections import namedtuple
 
+import cv2
+import numpy as np
 import torch
-from torch.autograd import Variable
 import torch.nn as nn
 import torch.nn.functional as F
-
 import torch.optim as optim
-import numpy as np
+from torch.autograd import Variable
 
-import cv2
 
-import math
+class MushaNetwork(nn.Module):
+    OUTPUT_AUGMENT = 10
 
-class GameNetwork(nn.Module):
-    OUTPUT_AUGMENT = 5
-
-    WorldState = namedtuple("WorldState", "actions observations world_image")
+    WorldState = namedtuple("WorldState", "world_image")
 
     def __init__(self,
-                 actions_size,
-                 actions_history_size,
                  possible_actions,
-                 observation_size,
-                 image_height=0,
-                 image_width=0):
-        super(GameNetwork, self).__init__()
+                 debug=False):
+        super(MushaNetwork, self).__init__()
 
-        actions_input_size = actions_size * actions_history_size
+        self.debug = debug
+        self.has_cuda = torch.cuda.is_available()
 
-        self.actions_to_hidden = nn.Linear(actions_input_size, actions_history_size)
-        hidden_input_size = actions_history_size
-
-        self.observation_to_hidden = nn.Linear(observation_size, observation_size * actions_input_size)
-        hidden_input_size += (observation_size * actions_input_size)
+        self.max_pool_shrink_1_2 = torch.nn.MaxPool2d(2, 2)
+        self.max_pool_conv_a = torch.nn.MaxPool2d((2, 2), (1, 2))
+        self.max_pool_conv_b = torch.nn.MaxPool2d(4, (2, 1))
+        self.max_pool_conv_c = torch.nn.MaxPool2d((2, 3), 2)
 
         # Image
-        self.shrink_1_4 = nn.Conv2d(4, 4, kernel_size=6, stride=2, padding=1)
-        self.batch_norm_4chan_1 = nn.BatchNorm2d(4)
+        self.conv_input_a = nn.Conv2d(4, 8, kernel_size=4)
+        self.conv_input_a_bn = nn.BatchNorm2d(8)
 
-        self.shrink_1_2 = nn.Conv2d(4, 4, kernel_size=4, stride=2, padding=1)
-        self.batch_norm_4chan_2 = nn.BatchNorm2d(4)
+        self.conv_input_b = nn.Conv2d(8, 16, kernel_size=5)
+        self.conv_input_b_bn = nn.BatchNorm2d(16)
 
-        self.conv_layer_a = nn.Conv2d(4, 16, kernel_size=5, stride=2)
-        self.batch_norm_16chan = nn.BatchNorm2d(16)
+        self.conv_layer_a = nn.Conv2d(16, 32, kernel_size=(2, 3))
+        self.conv_layer_a_bn = nn.BatchNorm2d(32)
 
-        self.conv_layer_b = nn.Conv2d(16, 32, kernel_size=5, stride=2)
-        self.batch_norm_32chan_1 = nn.BatchNorm2d(32)
+        self.conv_layer_b = nn.Conv2d(32, 128, kernel_size=(3, 2))
+        self.conv_layer_b_bn = nn.BatchNorm2d(128)
 
-        self.conv_layer_c = nn.Conv2d(32, 128, kernel_size=5, stride=2)
-        self.batch_norm_32chan_2 = nn.BatchNorm2d(128)
-        hidden_input_size += 896
-        hidden_input_size = 4964
-        self.hidden_to_out_hidden = nn.Linear(hidden_input_size, possible_actions * self.OUTPUT_AUGMENT)
+        self.conv_layer_c = nn.Conv2d(128, 256, kernel_size=5)
+        self.conv_layer_c_bn = nn.BatchNorm2d(256)
 
+        hidden_input_size = 32256
+        self.hidden_layer = nn.Linear(hidden_input_size, possible_actions * self.OUTPUT_AUGMENT)
         self.output_layer = nn.Linear(possible_actions * self.OUTPUT_AUGMENT, possible_actions)
 
         self.optimizer = None
         self.loss_fn = None
 
-    def process_state(self, actions_history, memory_observations, world_image):
-        #world_image = cv2.resize(world_image, (80, 60))
-
+    def process_state(self, world_image):
         b, g, r = cv2.split(world_image)
         grey = cv2.cvtColor(world_image, cv2.COLOR_BGR2GRAY)
 
         imgarray = np.array([b, g, r, grey]).astype(np.float32)
 
-        return self.WorldState(np.ndarray.flatten(np.array(actions_history)).astype(dtype=np.float32),
-                               np.array(memory_observations, dtype=np.float32),
-                               imgarray)
+        return self.WorldState(imgarray)
 
     def forward(self, world_states):
         # Tensors from inputs
-        actions, observations, world_images = map(np.array, zip(*world_states))
-        #actions = Variable(torch.from_numpy(actions).cuda(), requires_grad=True).cuda()
-        #observations = Variable(torch.from_numpy(observations).cuda(), requires_grad=True).cuda()
-        #world_images = Variable(torch.from_numpy(world_images).cuda(), requires_grad=True).cuda()
-
-        actions = Variable(torch.from_numpy(actions), requires_grad=True)
-        observations = Variable(torch.from_numpy(observations), requires_grad=True)
+        world_images = np.array(list(zip(*world_states)))[0]
         world_images = Variable(torch.from_numpy(world_images), requires_grad=True)
+        if self.has_cuda:
+            world_images = world_images.cuda()
 
-        conv_1_4 = F.relu(self.batch_norm_4chan_1(self.shrink_1_4(world_images)))
+        conv_output = F.relu(self.max_pool_shrink_1_2(self.conv_input_a_bn(self.conv_input_a(world_images))))
 
-        channels = conv_1_4[0].detach().numpy()
+        if self.debug:
+            channels = conv_output[0].detach().numpy()
 
-        channels = [np.concatenate( (channels[0:2]) ),np.concatenate((channels[2:]))]
-        channels = np.concatenate(channels,axis=1)
-        cv2.imshow("First conv",channels)
+            channels = [np.concatenate(channels[i:i + 2]) for i in range(8 // 2)]
+            channels = np.concatenate(channels, axis=1)
+            cv2.imshow("First 2dconv", channels)
 
-        conv_1_2 = F.relu(self.batch_norm_4chan_2(self.shrink_1_2(conv_1_4)))
+        conv_output = F.relu(self.max_pool_shrink_1_2(self.conv_input_b_bn(self.conv_input_b(conv_output))))
 
-        channels = conv_1_2[0].detach().numpy()
+        if self.debug:
+            channels = conv_output[0].detach().numpy()
+            channels = [np.concatenate(channels[i:i + 4]) for i in range(16 // 4)]
+            channels = np.concatenate(channels, axis=1)
+            cv2.imshow("Second 2dconv", channels)
 
-        channels = [np.concatenate( (channels[0:2]) ),np.concatenate((channels[2:]))]
-        channels = np.concatenate(channels,axis=1)
-        cv2.imshow("Second conv",channels)
-        
+        conv_output = F.relu(self.max_pool_conv_a(self.conv_layer_a_bn(self.conv_layer_a(conv_output))))
 
-        conv_output = F.relu(self.batch_norm_16chan(self.conv_layer_a(conv_1_2)))
+        if self.debug:
+            channels = conv_output[0].detach().numpy()
+            channels = [np.concatenate(channels[i:i + 4]) for i in range(32 // 4)]
+            channels = np.concatenate(channels, axis=1)
+            cv2.imshow("Trird conv", channels)
 
-        channels = conv_output[0].detach().numpy()
-        channels = [ np.concatenate(channels[i:i+4]) for i in range(16//4) ]
-        channels = np.concatenate(channels,axis=1)
-        cv2.imshow("Trird conv",channels)
+        conv_output = F.relu(self.max_pool_conv_b(self.conv_layer_b_bn(self.conv_layer_b(conv_output))))
 
-        conv_output = F.relu(self.batch_norm_32chan_1(self.conv_layer_b(conv_output)))
+        if self.debug:
+            channels = conv_output[0].detach().numpy()
+            channels = [np.concatenate(channels[i:i + 16]) for i in range(128 // 16)]
+            channels = np.concatenate(channels, axis=1)
+            cv2.imshow("Fourth conv", channels)
 
-        channels = conv_output[0].detach().numpy()
-        channels = [ np.concatenate(channels[i:i+4]) for i in range(32//4) ]
-        channels = np.concatenate(channels,axis=1)
-        cv2.imshow("Fourth conv",channels)
+        conv_output = F.relu(self.max_pool_conv_c(self.conv_layer_c_bn(self.conv_layer_c(conv_output))))
 
-        conv_output = F.relu(self.batch_norm_32chan_2(self.conv_layer_c(conv_output)))
+        if self.debug:
+            channels = conv_output[0].detach().numpy()
+            channels = [np.concatenate(channels[i:i + 16]) for i in range(256 // 16)]
+            channels = np.concatenate(channels, axis=1)
+            cv2.imshow("Fifth conv", channels)
 
+        if self.debug:
+            cv2.waitKey(1)
 
-        cv2.waitKey(1)
-
-        # Linear Layers
-        actions_hidden = self.actions_to_hidden(actions)
-        observations_hidden = self.observation_to_hidden(observations)
-
-        #print("Conv out =", conv_output.shape)
-
-        hidden = torch.cat((actions_hidden,
-                            observations_hidden,
-                            conv_output.view(conv_output.size(0), -1)), dim=1)
-
-        out_hidden = self.hidden_to_out_hidden(hidden)
-        return self.output_layer(out_hidden)
+        hidden = F.relu(self.hidden_layer(conv_output.view(conv_output.size(0), -1)))
+        return self.output_layer(hidden)
 
     def get_best_action(self, world_state):
-        #return self([world_state]).cuda().max(1)[1].view(1, 1)
+        if self.has_cuda:
+            return self([world_state]).cuda().max(1)[1].view(1, 1)
         return self([world_state]).max(1)[1].view(1, 1)
 
 
@@ -140,18 +123,20 @@ class GameNetworkTrainer:
     Replay = namedtuple("Replay", "state action reward next_state")
     GAMMA = 0.983
 
-    def __init__(self, actions_size,
-                 actions_history_size,
+    def __init__(self,
                  possible_actions,
                  observation_size,
                  replays_memory=550,
                  sample_batch_size=131,
-                 save_path = None):
-        self.policy_network = GameNetwork(actions_size, actions_history_size, possible_actions, observation_size)
-        self.target_network = GameNetwork(actions_size, actions_history_size, possible_actions, observation_size)
+                 save_path=None):
+
+        self.has_cuda = torch.cuda.is_available()
+
+        self.policy_network = MushaNetwork(possible_actions, observation_size)
+        self.target_network = MushaNetwork(possible_actions, observation_size)
 
         if save_path:
-            self.policy_network.load_state_dict(torch.load(save_path,map_location='cpu'))
+            self.policy_network.load_state_dict(torch.load(save_path, map_location='cpu'))
             self.policy_network.eval()
 
         self.target_network.load_state_dict(self.policy_network.state_dict())
@@ -178,27 +163,32 @@ class GameNetworkTrainer:
         batch = random.sample(self.replays, self.sample_batch_size)
         old_state_batch, action_batch, reward_batch, next_state_batch = map(np.array, zip(*batch))
 
-        #action_batch = torch.from_numpy(action_batch).cuda().view(-1, 1)
         action_batch = torch.from_numpy(action_batch).view(-1, 1)
+        if self.has_cuda:
+            action_batch = action_batch.cuda()
 
         predicted_values = self.policy_network(old_state_batch).gather(1, action_batch)
         next_predicted_values = self.target_network(next_state_batch).max(1)[0]
 
-        #expected_values = (next_predicted_values * self.GAMMA).double() + torch.from_numpy(reward_batch).cuda().double()
-        expected_values = (next_predicted_values * self.GAMMA).double() + torch.from_numpy(reward_batch).double()
+        if self.has_cuda:
+            expected_values = (next_predicted_values * self.GAMMA).double() + torch.from_numpy(
+                reward_batch).double().cuda()
+        else:
+            expected_values = (next_predicted_values * self.GAMMA).double() + torch.from_numpy(reward_batch).double()
 
-        #loss = self.loss_fn(predicted_values.double().cuda(), expected_values)
+        if self.has_cuda:
+            predicted_values = predicted_values.cuda()
+
         loss = self.loss_fn(predicted_values.double(), expected_values)
         self.optimizer.zero_grad()
         loss.backward()
-        for p in self.policy_network.parameters():
-            p.grad.data.clamp_(-1, 1)
         self.optimizer.step()
-
-        return loss
 
     def get_policy_network(self):
         return self.policy_network
 
     def update_target(self):
         self.target_network.load_state_dict(self.policy_network.state_dict())
+
+    def store_policy(self, path):
+        torch.save(self.policy_network().state_dict(), path)
